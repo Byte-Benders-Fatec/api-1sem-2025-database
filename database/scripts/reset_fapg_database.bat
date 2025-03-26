80f1aa49f6a53f1106c7e83090714243b7a3beac
Joniel R. de Oliveira <joniel.oliveira@fatec.sp.gov.br>
2025-03-26 17:09:56 -0300

    ✨ feat: add triggers in .bat
    Add triggers in .bat.

--- database/scripts/reset_fapg_database.bat
+++ database/scripts/reset_fapg_database.bat
@@ -5,13 +5,14 @@
 set USUARIO=root
 set /p SENHA="Digite sua senha MySQL: "
 
-:: Caminho para o executável do MySQL (ajuste se necessário)
+:: Caminho para o executável do MySQL
 set MYSQL_BIN="C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe"
 
-:: Caminho dos arquivos SQL (ajuste conforme seu projeto)
-set SCHEMA=01_schema.sql
-set SEED=02_seed.sql
+:: Caminho dos arquivos SQL
 set RESET=00_reset.sql
+set SCHEMA=01_schema.sql
+set TRIGGERS=02_triggers.sql
+set SEED=03_seed.sql
 
 echo.
 echo ========================
@@ -24,6 +25,12 @@ echo ========================
 echo Criando schema
 echo ========================
 %MYSQL_BIN% -u %USUARIO% -p%SENHA% < %SCHEMA%
+
+echo.
+echo ========================
+echo Criando triggers
+echo ========================
+%MYSQL_BIN% -u %USUARIO% -p%SENHA% < %TRIGGERS%
 
 echo.
 echo ========================
